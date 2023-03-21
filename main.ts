eb406e7c100ee2ddb6e18f7425d6dfe6b89912af
Dawit-Fikru <123103112+Dawit-Fikru@users.noreply.github.com>
2023-03-21 09:17:11 -0700

    new file for changes

--- main.ts
+++ main.ts
@@ -16,42 +16,31 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
     otherSprite.destroy(effects.disintegrate)
     info.changeScoreBy(1)
 })
+// info.changeLifeBy(-1)
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     scene.cameraShake(4, 500)
     otherSprite.destroy(effects.disintegrate)
     sprite.startEffect(effects.fire, 200)
-    //info.changeLifeBy(-1)
 })
-
 let projectile: Sprite = null
 let ship: Sprite = null
-let asteroids = [
-sprites.space.spaceSmallAsteroid1,
-sprites.space.spaceSmallAsteroid0,
-]
-
+let asteroids = [sprites.space.spaceSmallAsteroid1, sprites.space.spaceSmallAsteroid0]
 ship = sprites.create(img`
-    . . 1 . .
-    . 1 1 1 .
-    1 1 1 1 1
-    . . 4 . .
-    . . . . .
-`, SpriteKind.Player)
+    . . 1 . . 
+    . 1 1 1 . 
+    1 1 1 1 1 
+    . . 4 . . 
+    . . . . . 
+    `, SpriteKind.Player)
 ship.setStayInScreen(true)
 ship.bottom = 12
 controller.moveSprite(ship, 100, 100)
 info.setLife(3)
 effects.starField.startScreenEffect()
-
-game.onUpdateInterval(500, function () {
-    projectile = sprites.createProjectileFromSide(asteroids[randint(0, asteroids.length - 1)], 0, 75)
-    projectile.setKind(SpriteKind.Enemy)
-    projectile.x = randint(10, 150)
-})
-game.onUpdate(function(){
+game.onUpdate(function () {
     if (ship.x < 10) {
         ship.x = screen.width - 11
-    } else if(ship.x > screen.width - 10){
+    } else if (ship.x > screen.width - 10) {
         ship.x = 11
     }
     if (ship.y < 10) {
@@ -59,4 +48,9 @@ game.onUpdate(function(){
     } else if (ship.y > screen.height - 10) {
         ship.y = 11
     }
+})
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(asteroids[randint(0, asteroids.length - 1)], 0, 75)
+    projectile.setKind(SpriteKind.Enemy)
+    projectile.x = randint(10, 150)
 })
