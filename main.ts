f928e9f20867b354c00a196047dcfab7b1188b8e
Dawit-Fikru <123103112+Dawit-Fikru@users.noreply.github.com>
2023-03-21 09:27:07 -0700

    tilemap updated/push testing

--- main.ts
+++ main.ts
@@ -16,41 +16,27 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
     otherSprite.destroy(effects.disintegrate)
     info.changeScoreBy(1)
 })
-// info.changeLifeBy(-1)
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     scene.cameraShake(4, 500)
     otherSprite.destroy(effects.disintegrate)
     sprite.startEffect(effects.fire, 200)
+    info.changeLifeBy(-1)
 })
+let projectile2: Sprite = null
 let projectile: Sprite = null
 let ship: Sprite = null
+tiles.setCurrentTilemap(tilemap`level1`)
 let asteroids = [sprites.space.spaceSmallAsteroid1, sprites.space.spaceSmallAsteroid0]
-ship = sprites.create(img`
-    . . 1 . . 
-    . 1 1 1 . 
-    1 1 1 1 1 
-    . . 4 . . 
-    . . . . . 
-    `, SpriteKind.Player)
+ship = sprites.create(sprites.space.spaceRedShip, SpriteKind.Player)
 ship.setStayInScreen(true)
-ship.bottom = 12
+ship.bottom = 120
 controller.moveSprite(ship, 100, 100)
 info.setLife(3)
 effects.starField.startScreenEffect()
-game.onUpdate(function () {
-    if (ship.x < 10) {
-        ship.x = screen.width - 11
-    } else if (ship.x > screen.width - 10) {
-        ship.x = 11
-    }
-    if (ship.y < 10) {
-        ship.y = screen.height - ship.height
-    } else if (ship.y > screen.height - 10) {
-        ship.y = 11
-    }
-})
 game.onUpdateInterval(500, function () {
-    projectile = sprites.createProjectileFromSide(asteroids[randint(0, asteroids.length - 1)], 0, 75)
+    projectile = sprites.createProjectileFromSide(asteroids[randint(0, asteroids.length - 1)], randint(-15, 15), 75)
+    projectile2 = sprites.createProjectileFromSprite(asteroids[randint(0, asteroids.length - 1)], null, 50, 50)
     projectile.setKind(SpriteKind.Enemy)
+    projectile2.setKind(SpriteKind.Enemy)
     projectile.x = randint(10, 150)
 })
