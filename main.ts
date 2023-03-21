14a16591a461bc14322cfdf96d239bfa9a3a2f3d
vivid-milk <56522561+vivid-milk@users.noreply.github.com>
2023-03-21 09:10:53 -0700

    added ship teleport

--- main.ts
+++ main.ts
@@ -20,26 +20,43 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     scene.cameraShake(4, 500)
     otherSprite.destroy(effects.disintegrate)
     sprite.startEffect(effects.fire, 200)
-    info.changeLifeBy(-1)
+    //info.changeLifeBy(-1)
 })
+
 let projectile: Sprite = null
 let ship: Sprite = null
 let asteroids = [
 sprites.space.spaceSmallAsteroid1,
 sprites.space.spaceSmallAsteroid0,
-sprites.space.spaceAsteroid0,
-sprites.space.spaceAsteroid1,
-sprites.space.spaceAsteroid4,
-sprites.space.spaceAsteroid3
 ]
-ship = sprites.create(sprites.space.spaceRedShip, SpriteKind.Player)
+
+ship = sprites.create(img`
+    . . 1 . .
+    . 1 1 1 .
+    1 1 1 1 1
+    . . 4 . .
+    . . . . .
+`, SpriteKind.Player)
 ship.setStayInScreen(true)
-ship.bottom = 120
+ship.bottom = 12
 controller.moveSprite(ship, 100, 100)
 info.setLife(3)
 effects.starField.startScreenEffect()
+
 game.onUpdateInterval(500, function () {
     projectile = sprites.createProjectileFromSide(asteroids[randint(0, asteroids.length - 1)], 0, 75)
     projectile.setKind(SpriteKind.Enemy)
     projectile.x = randint(10, 150)
+})
+game.onUpdate(function(){
+    if (ship.x < 10) {
+        ship.x = screen.width - 11
+    } else if(ship.x > screen.width - 10){
+        ship.x = 11
+    }
+    if (ship.y < 10) {
+        ship.y = screen.height - ship.height
+    } else if (ship.y > screen.height - 10) {
+        ship.y = 11
+    }
 })
